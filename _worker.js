d3ec489ae3d9561025e3adcc2bdd2a1563f3fe7d
fzminato <137714583+fzminato@users.noreply.github.com>
2024-08-04 01:05:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'd085c4d4-8075-4ce7-bb2c-67f51c3cd038';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
